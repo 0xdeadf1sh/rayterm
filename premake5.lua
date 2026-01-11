172e8a5d4fab03a1f14bd3014cd6982f96aae0e5
0xdeadf1sh <0xdeadf1sh@tuta.io>
2026-01-11 20:28:13 +0400

    Clear background to dark-blue

--- premake5.lua
+++ premake5.lua
@@ -1,24 +1,157 @@
 workspace "rayterm"
-    configurations { "debug", "release" }
+
+    configurations {
+        "debug",
+        "release",
+        "tsan",
+    }
+
     location "build"
 
 project "rayterm"
     kind "ConsoleApp"
     language "C"
+
     targetdir "build/%{cfg.buildcfg}"
 
     files {
-        "src/main.c"
+        "src/main.c",
+    }
+
+    includedirs {
+        "include"
     }
 
     links {
-        "notcurses-core"
+        "notcurses-core",
     }
 
+    defines {
+        "_DEFAULT_SOURCE",
+        "_XOPEN_SOURCE=600",
+    }
+
+    -- minimum: GCC 15
+    filter { "system:linux", "action:gmake" }
+        buildoptions {
+            "`pkg-config --cflags notcurses-core`",
+            "-std=c23",
+            "-g",
+            "-save-temps",
+            "-march=native",
+            "-Wall",
+            "-Wextra",
+            "-Wpedantic",
+            "-Whardened",
+            "-Werror",
+            "-Wformat",
+            "-Wformat=2",
+            "-Wformat-security",
+            "-Wconversion",
+            "-Wpadded",
+            "-Wsign-conversion",
+            "-Wimplicit-fallthrough",
+            "-Wfloat-equal",
+            "-Wtrampolines -fzero-init-padding-bits=all",
+            "-Wbidi-chars=any",
+            "-Wundef",
+            "-Wshadow",
+            "-Wpointer-arith",
+            "-Wcast-align",
+            "-Wstrict-prototypes",
+            "-Wmissing-prototypes",
+            "-Wmissing-declarations",
+            "-Wredundant-decls",
+            "-Wold-style-definition",
+            "-Wwrite-strings",
+            "-Waggregate-return",
+            "-Wcast-qual",
+            "-Wswitch-default",
+            "-Wunreachable-code",
+            "-Winit-self",
+            "-Wimplicit-function-declaration",
+            "-Wdeprecated",
+            "-U_FORTIFY_SOURCE",
+            "-D_FORTIFY_SOURCE=3",
+            "-fstrict-flex-arrays=3",
+            "-fstack-clash-protection",
+            "-fstack-protector-strong",
+            "-Wl,-z,nodlopen",
+            "-Wl,-z,noexecstack",
+            "-Wl,-z,relro",
+            "-Wl,-z,now",
+            "-Wl,--as-needed",
+            "-Wl,--no-copy-dt-needed-entries",
+            "-fPIE -pie",
+            "-fcf-protection=full", -- x86_64
+            -- "-mbrach-protection=standard", -- aarch64
+            "-fno-delete-null-pointer-checks",
+            "-fno-strict-overflow",
+            "-fno-strict-aliasing",
+            "-ftrivial-auto-var-init=zero",
+            "-fverbose-asm",
+            "-masm=intel",
+        }
+
+        linkoptions {
+            "`pkg-config --libs notcurses-core`",
+            "-Wl,-z,nodlopen",
+            "-Wl,-z,noexecstack",
+            "-Wl,-z,relro",
+            "-Wl,-z,now",
+            "-Wl,--as-needed",
+            "-Wl,--no-copy-dt-needed-entries",
+            "-fPIE -pie",
+        }
+
     filter "configurations:debug"
-        defines { "_DEBUG" }
         symbols "On"
 
+        defines {
+            "_DEBUG",
+        }
+
+        buildoptions {
+            "-O1",
+            "-fno-omit-frame-pointer",
+            "-fno-optimize-sibling-calls",
+            "-fno-common",
+            "-fsanitize=address,undefined",
+        }
+
+        linkoptions {
+            "-fsanitize=address,undefined",
+        }
+
     filter "configurations:release"
-        defines { "NDEBUG" }
-        optimize "On"
+        symbols "On"
+
+        defines {
+            "NDEBUG",
+        }
+
+        buildoptions {
+            "-O2",
+            "-fsanitize-trap=undefined",
+        }
+
+        linkoptions {
+            "-fsanitize-trap=undefined",
+            "-flto",
+        }
+
+    filter "configurations:tsan"
+        symbols "On"
+
+        defines {
+            "NDEBUG",
+        }
+
+        buildoptions {
+            "-O2",
+            "-fsanitize=thread",
+        }
+
+        linkoptions {
+            "-fsanitize=thread",
+        }
--- src/main.c
+++ src/main.c
@@ -1,8 +1,69 @@
+#include <notcurses/notcurses.h>
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <locale.h>
 
-int main(void)
+int main([[maybe_unused]] int argc, char** argv)
 {
-    printf("hello, world!\n");
+    if (!setlocale(LC_ALL, "")) {
+        fprintf(stderr, "%s: setlocale(LC_ALL, \"\") failed\n", argv[0]);
+        return EXIT_FAILURE;
+    }
 
-    return 0;
+    struct notcurses_options opts = {};
+    struct notcurses* nc = notcurses_core_init(&opts, stdout);
+    if (!nc) {
+        fprintf(stderr, "%s: notcurses_init() failed\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    uint32_t rows = 0;
+    uint32_t cols = 0;
+    struct ncplane* nstd = notcurses_stddim_yx(nc, &rows, &cols);
+    if (!nstd) {
+        fprintf(stderr, "%s: notcurses_stdplane() failed\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    size_t pixelBufferSize = rows * cols * sizeof(uint32_t);
+    uint32_t* rgb = malloc(pixelBufferSize);
+    if (!rgb) {
+        fprintf(stderr, "%s: malloc() failed\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    for (uint32_t i = 0; i < rows * cols; ++i) {
+        rgb[i] = 0xFF332211;
+    }
+
+    struct ncvisual_options vopts = {};
+    vopts.n = nstd;
+    vopts.leny = rows;
+    vopts.lenx = cols;
+    vopts.blitter = NCBLIT_1x1;
+    vopts.scaling = NCSCALE_NONE;
+
+    if (-1 == ncblit_rgba(rgb, (int32_t)(cols * sizeof(uint32_t)), &vopts)) {
+        fprintf(stderr, "%s: ncblit_rgba() failed\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    if (-1 == notcurses_render(nc)) {
+        fprintf(stderr, "%s: notcurses_render() failed\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    for (;;) {
+        struct ncinput input = {};
+        notcurses_get(nc, NULL, &input);
+
+        if (input.id == 'q') {
+            break;
+        }
+    }
+    free(rgb);
+    return notcurses_stop(nc);
 }
